b4250364c3c93f971dfa246e6c347096f64ea275
deepaghimire <deepaghimire50@gmail.com>
2025-03-13 15:25:52 +0545

    react

--- day-3-js/ffff.js
+++ day-3-js/ffff.js
@@ -3,10 +3,96 @@
 //  const checkData = a %2==0 ?"is even": "is odd";
 //  console.log(checkData);
 
- //given an array of numbers 
- let aa=[1,2,3,4,5];
-  const b =aa.map(aa=> aa*aa);
-  
-  console.log(b);
+//given an array of numbers 
+//  let aa=[1,2,3,4,5];
+//   const b =aa.map(aa=> aa*aa);
+// //   console.log(b.filter(b<10))  console.log(b);
+
+
+
+
+
+// //dynamic default parameter/value 
+//traditional
+// function Greetuser(name="guest" ) {
+//     return `hello ${name}`
+// }
+// console.log(Greetuser( ));
+//no destructured
+// function add1 (a1,b1) {
+//     return a1+b1;
+//     }
+// console.log(add1(10,15));
+
+// const add = ({a,b}) => a+b
+// console.log(add({a:10,b:15}));
+
+
+// //arrow  function
+// const Greet=(name1="user") => `hello ${name1}`
+// console.log(Greet());
+
+// //for loop
+// for (let i=2; i<= 5; i++)
+//    {
+//     console.log(i);
+//    }
+// //while loop
+// let j=2;
+
+// while(j<=6){
+//     console.log(j)
+// j++;
+// }
+
+// //dowhile loop
+// let k=2;
+// do 
+// {
+//     console.log(k);
+//     k++;
+// }
+// while (k<=6);
+
+
+// const fruits =["apple","orange","gauva"]
+// console.log(fruits.length)
+
+
+console.log("1.............................")
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("2....................................")
+let j = 10;
+
+while (j >= 1) {
+    console.log(j)
+    j--;
+}
+console.log("3.......................................................")
+
+let k = 1;
+do {
+    console.log(k);
+    k++;
+}
+while (k <= 5);
+
+console.log("4.................................................................")
+
+ let colors = ["Red", "Blue", "Green"];
+colors.forEach((color) => console.log(color));
+
+//slice 
+//include
+//push 
+//pop
+//concat
+//shift
+//unshift
+//splice
+
 
 
